038d299e115e14df1de8570ba03ae70a666fa6a7
michaelmuther <mgmuther@gmail.com>
2023-08-29 07:32:56 +0200

    add comments to InputMain file

--- src/main/java/com/michaelmuther/input/InputMain.java
+++ src/main/java/com/michaelmuther/input/InputMain.java
@@ -7,7 +7,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -28,49 +27,40 @@ public class InputMain {
         String company;
         LocalDate date;
 
-        String input_trial_balancesFolderPath = "src/input_trial_balances";
-        Path directoryPath = Paths.get(input_trial_balancesFolderPath);
+        String INPUT_TRIAL_BALANCE_FOLDER = "src/input_trial_balances";
+        Path directoryPath = Paths.get(INPUT_TRIAL_BALANCE_FOLDER);
 
-        try {
-            Files.list(directoryPath)
-                    .filter(Files::isRegularFile)
-                    .forEach(file -> System.out.println("File: " + file.getFileName()));
+        Files.list(directoryPath) // this needs to be in a try block
+                .filter(Files::isRegularFile)
+                .forEach(file -> System.out.println("File: " + file.getFileName()));
 
-            FileInputStream file = new FileInputStream(new File("src/input_trial_balances/TESTUS01.xlsx"));
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            Row headerRow = sheet.getRow(0);
+        FileInputStream file = new FileInputStream("src/input_trial_balances/TESTUS01.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(file); // this needs to be in a try block
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        Row headerRow = sheet.getRow(0);
 
-            company = headerRow.getCell(1).getStringCellValue();
-            date = headerRow.getCell(3).getLocalDateTimeCellValue().toLocalDate();
+        company = headerRow.getCell(1).getStringCellValue();
+        date = headerRow.getCell(3).getLocalDateTimeCellValue().toLocalDate();
 
-            System.out.println(company);
-            System.out.println(date);
+        System.out.println(company);
+        System.out.println(date);
 
-            Iterator<Row> rowIterator = sheet.iterator();
-            rowIterator.next(); // skip first row (0)
-            rowIterator.next(); // skip second row (1)
+        Iterator<Row> rowIterator = sheet.iterator();
+        rowIterator.next(); // skip first row (0)
+        rowIterator.next(); // skip second row (1)
 
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-                int number = (int) row.getCell(0).getNumericCellValue();
-                String name = row.getCell(1).getStringCellValue();
-                BigDecimal balance = BigDecimal.valueOf(row.getCell(2).getNumericCellValue());
-                accounts.put(number, new GLAccount(number, name, balance));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            int number = (int) row.getCell(0).getNumericCellValue();
+            String name = row.getCell(1).getStringCellValue();
+            BigDecimal balance = BigDecimal.valueOf(row.getCell(2).getNumericCellValue());
+            accounts.put(number, new GLAccount(number, name, balance));
         }
+
         trialBalances.add(new SourceTrialBalance(company, date, accounts));
 
-
-
-        var testConsolidator = new Consolidator(trialBalances);
-        var testConsolidatedTB = testConsolidator.consolidate();
-        testConsolidatedTB.printTrialBalance();
+//        var testConsolidator = new Consolidator(trialBalances);
+//        var testConsolidatedTB = testConsolidator.consolidate();
+//        testConsolidatedTB.printTrialBalance();
     }
-
-
-
-
 }
